b1c26d174e94b522f4cc23ca960af07fcfab6c49
Vinícius Colazelli <119797073+ViniciusColazelli@users.noreply.github.com>
2026-04-25 16:07:47 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Eu Consegui subir minha pagina pra internet 😎</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identeação errada");
-}
-
 export default Home;
