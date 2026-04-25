da868579b2913836cf2ae3af41bbdc0ce55c9782
Vinícius Colazelli <119797073+ViniciusColazelli@users.noreply.github.com>
2026-04-25 15:57:09 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de estudo do https://curso.dev para aprender a como criar projetos do zero
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Eu Consegui subir minha pagina pra internet 😎</h1>
+function Home() {
+  return <h1>Eu Consegui subir minha pagina pra internet 😎</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identeação errada");
 }
 
 export default Home;
